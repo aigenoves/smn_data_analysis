a43afa3cf64206dffe88b71a4b279a48f54e4ca0
Agustin Genoves <agustingenoves@hotmail..com>
2024-09-12 20:17:58 -0300

    fix: Fixed downloading of already downloaded data
    Now it does not download the txt with the daily data again if the daily file is already downloaded.

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+
 def generate_dates_until_today(start_year: int) -> list[str]:
     """
     Genera una lista de fechas en el formato YYYYMMDD desde el 1 de enero del año dado hasta hoy.
@@ -9,7 +10,7 @@ def generate_dates_until_today(start_year: int) -> list[str]:
     """
     start_date = datetime(start_year, 1, 1)
     end_date = datetime.now()
-    
+
     dates = []
     current_date = start_date
 
